35e6e4d544546d0fafe8d838db88c0cf64b98eef
Matias <matias@Matiass-MacBook-Pro.local>
2020-06-15 15:40:23 -0300

    added types  styles

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
     }
     fetchData();
   }, [dispatch, pokename]);
-
+  
   return (
     <Container>
       <SearchBar />
--- src/components/pokeCard/PokeCard.jsx
+++ src/components/pokeCard/PokeCard.jsx
@@ -1,13 +1,75 @@
 import React from 'react';
 import './pokeCard.css';
 import { useSelector } from 'react-redux';
-import PropTypes from 'prop-types';
 
 const PokeCard = () => {
 
     const pokemonInfo = useSelector((state) => state.getPokeData.pokeData)
 
     const pokeImage = `https://pokeres.bastionbot.org/images/pokemon/${pokemonInfo.id}.png`
+
+    const colorType = () => {
+        
+        let type= '';
+
+        switch(pokemonInfo.types[0].type.name) {
+            case 'grass':
+                type = 'grass'
+                break;
+            case 'normal':
+                type = 'normal'
+                break;
+            case 'fighting':
+                type = 'fighting'
+                break;
+            case 'flying':
+                type = 'flying'
+                break;
+            case 'poison':
+                type = 'poison'
+                break;
+            case 'rock':
+                type = 'rock'
+                break;
+            case 'bug':
+                type = 'bug'
+                break;
+            case 'ghost':
+                type = 'ghost'
+                break;
+            case 'steel':
+                type = 'steel'
+                break;
+            case 'fire':
+                type = 'fire'
+                break;
+            case 'water':
+                type = 'water'
+                break;
+            case 'electric':
+                type = 'electric'
+                break;
+            case 'psychic':
+                type = 'psychic'
+                break;
+            case 'ice':
+                type = 'ice'
+                break;
+            case 'dragon':
+                type = 'dragon'
+                break;
+            case 'dark':
+                type = 'dark'
+                break;
+            case 'fairy':
+                type = 'fairy'
+                break;
+            default:
+                break;
+        }
+
+        return type;
+    }
 
     return(
         <div className='container'>
@@ -24,26 +86,26 @@ const PokeCard = () => {
                             <img src={pokeImage} alt='pokemon' id='pokemon-img'/>
                         </div>
                         <div className='data-container'>
-                            <div className='poke-info'>
+                            <div className={colorType()}>
                                 <p>Name: {pokemonInfo.name}</p>
                             </div>
-                            <div className='poke-info'>
+                            <div className={colorType()}>
                                 <p>ID: {pokemonInfo.id}</p>
                             </div>
-                            <div className='poke-info'>
-                                <p>Type: </p>
+                            <div className={colorType()}>
+                                <p>Type: {pokemonInfo.types[0].type.name}</p>
                             </div>
-                            <div className='poke-info'>
-                                <p>Evolves to: </p>
+                            <div className={colorType()}>
+                                <p>Weight: {pokemonInfo.weight/10} kg</p>
                             </div>
-                            <div className='poke-info'>
-                                <p>Weight: </p>
+                            <div className={colorType()}>
+                                <p>Height: {pokemonInfo.height/10} m</p>
                             </div>
-                            <div className='poke-info'>
-                                <p>Height: </p>
+                            <div className={colorType()}>
+                                <p>Ability: {pokemonInfo.abilities[0].ability.name}</p>
                             </div>
-                            <div className='poke-info'>
-                                <p>Ability:</p>
+                            <div className={colorType()}>
+                                <p>Move: {pokemonInfo.moves[0].move.name}</p>
                             </div>
                         </div>
                         </>
@@ -52,14 +114,5 @@ const PokeCard = () => {
         </div>
     );
 }
-
-PokeCard.propTypes = {
-    
-};
-
-PokeCard.defaultProps = {
-    
-};
-
 
 export default PokeCard;
