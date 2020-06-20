47280329c734514ed51c4731ceca8cad1ee8b1f1
Matias <matias@Matiass-MacBook-Pro.local>
2020-06-20 20:30:22 -0300

    Updates

---
+++ precache-manifest.be5f7eb80e849ee7a479470f2a097532.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823cc6fa91779bcf2694e53be17cab9e",
+    "url": "/pokedex-app/index.html"
+  },
+  {
+    "revision": "94cf39475223f934c7b5",
+    "url": "/pokedex-app/static/css/main.769abe48.chunk.css"
+  },
+  {
+    "revision": "b6a2243b47d316cfb82f",
+    "url": "/pokedex-app/static/js/2.e6c5fee7.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/pokedex-app/static/js/2.e6c5fee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94cf39475223f934c7b5",
+    "url": "/pokedex-app/static/js/main.01c32b4b.chunk.js"
+  },
+  {
+    "revision": "f83011ee677a725a401a",
+    "url": "/pokedex-app/static/js/runtime-main.a5df2ecf.js"
+  }
+]);
